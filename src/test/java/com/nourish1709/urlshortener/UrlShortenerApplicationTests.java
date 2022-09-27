603ca7786fdf0d1be8a66ec04af47811f31b9f1c
Yevhenii Savonenko <yevs@jacobianengineering.com>
2022-09-27 07:50:36 +0300

    Initial commit

---
+++ src/test/java/com/nourish1709/urlshortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nourish1709.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
